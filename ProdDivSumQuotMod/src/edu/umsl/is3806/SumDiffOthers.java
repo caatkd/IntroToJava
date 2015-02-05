433fa569216d2289399b0cf1cc5fc465941f4fcb
caatkd <cjacosta90@gmail.com>
2015-02-05 16:53:12 -0600

    Java Paycheck Assignment
    I got bored and tried some stuff out.

---
+++ ProdDivSumQuotMod/src/edu/umsl/is3806/SumDiffOthers.java
@@ -0,0 +1,22 @@
+
+package edu.umsl.is3806;
+
+public class SumDiffOthers 
+{
+    int y = 200;
+    
+    public static void main(String[] args)
+    {
+       int y = 20;
+       SumDiffOthers sdo = new SumDiffOthers();
+       sdo.printY();
+       //System.out.println(this.y);
+       
+        }
+   public void printY()
+   {
+       int y = 20;
+       System.out.println(this.y);
+   }
+}
+
