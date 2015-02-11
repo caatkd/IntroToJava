4133d1f763fb81ba3301c8ee8b2e847c842c1570
caatkd <cjacosta90@gmail.com>
2015-02-10 19:55:36 -0600

    2/10/15 class updates

--- PayCheckCalc/src/paycheckcalc/PayCheckCalc.java
+++ PayCheckCalc/src/paycheckcalc/PayCheckCalc.java
@@ -5,6 +5,8 @@
  */
 package paycheckcalc;
 
+import java.util.Scanner;
+
 /**
  *
  * @author caatk_000
@@ -14,25 +16,45 @@ public class PayCheckCalc {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         // TODO code application logic here
+    float hours;
+    float payrate;
+    float otrate;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter your Hours: ");
+    hours = in.nextFloat();
     
-    int hours = 60;
-    int payrate = 9;
-    int ot = 2 * payrate* (hours-40);
-    int overtimehours = hours - 40;
-    int result = (hours - overtimehours)*9 +ot;
+    System.out.println("Enter your payrate: ");
+    payrate = in.nextFloat();
+    
+    
+   // int hours = 60;
+    //int payrate = 9;
+    //float ot = 2 * payrate* (hours-40);
+    //float overtimehours = hours - 40;
+    //float result = (hours - overtimehours)*9 +ot;
+    //int h;
+    //System.out.println("Enter your hours worked this pay period: ");
+    //h = in.nextInt();
+    
     
     if (hours <= 40){ 
+        float result = (hours *payrate);
         System.out.println("You worked "+ hours+" hours with no overtime hours.");
         System.out.println("Your pay rate is $"+ payrate + " an hour.");
         System.out.println("Your pay is $"+ result);
 
     }
     else { 
+        System.out.println("Enter your overtime multiplier: ");
+        otrate = in.nextFloat();
+        float overtimehours = hours - 40;
+        float ot = otrate * payrate* (hours-40);
+        float result = (hours - overtimehours)*payrate +ot;
         System.out.println("You worked " + hours+ " hours with "+ overtimehours +" overtime hours.");
         System.out.println("Your pay rate is $" + payrate + " an hour.");
-        System.out.println("Your overtime pay is double rates. Which is $"+ payrate*2+ " an hour.");
+        System.out.println("Your overtime pay is: "+otrate+".Which is $"+ payrate*otrate+ " an hour.");
         System.out.println("Your pay is $"+ result); 
     }
     //System.out.println("Pay with overtime is "+ hours * payrate + ot*9*(hours-40));
